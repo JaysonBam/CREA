cab348fdc31db37ed13b79214f1580abfcdbeff7
Jayson Bam <u23557894@tuks.co.za>
2025-09-22 20:50:05 +0200

    Render setup

--- backend/config/config.js
+++ backend/config/config.js
@@ -19,13 +19,25 @@ module.exports = {
     dialect: "postgres",
     logging: false,
   },
-  production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
-    dialect: "postgres",
-    logging: false,
-  },
+  production: process.env.DATABASE_URL
+    ? {
+        use_env_variable: "DATABASE_URL",
+        dialect: "postgres",
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        },
+        logging: false,
+      }
+    : {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
+        dialect: "postgres",
+        logging: false,
+      },
 };
